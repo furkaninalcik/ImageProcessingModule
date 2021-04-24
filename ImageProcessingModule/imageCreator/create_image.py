cc7a58440e04e81b02ed4e685228a8d69b726869
Muhammed Furkan İnalcık <m.furkaninalcik@gmail.com>
2021-04-24 07:37:50 +0300

    completed image processing functionality and storage connections

--- ImageProcessingModule/imageCreator/create_image.py
+++ ImageProcessingModule/imageCreator/create_image.py
@@ -10,15 +10,9 @@ from PIL import Image
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def download_blob():
+def download_blob(bucket_name, background_source_blob_name,  item_source_blob_name, bg_destination_file_name , item_destination_file_name):
     """Downloads a blob from the bucket."""
-    bucket_name = "image-processing-module.appspot.com"
-    background_source_blob_name = "input_images/harry_potter.jpg"
-    item_source_blob_name = "items/glasses1.png"
 
-    print(BASE_DIR)
-    bg_destination_file_name = os.path.join(BASE_DIR, "static_files/face_images/background_image.jpg")
-    item_destination_file_name = os.path.join(BASE_DIR, "static_files/items/item1.png")
 
 
     storage_client = storage.Client()
@@ -41,14 +35,58 @@ def download_blob():
         )
     )
 
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+
+
+
 # create combines the background image (containing faces) with items (mask, glasses etc.)
 def create(*args, **kwargs):
 
-    download_blob()
+    bucket_name = "image-processing-module.appspot.com"
+    background_source_blob_name = "input_images/harry_potter.jpg"
+    item_source_blob_name = "items/glasses1.png"
 
-    print(*args)
-    bg_destination_file_name = os.path.join(BASE_DIR, "imageCreator/face_images/downloadedFile.jpg")
-    path = "F:\ImageProcessingModule\ImageProcessingModule\ImageProcessingModule\imageCreator\face_images\downloadedFile.jpg"
+    bg_destination_file_name = os.path.join(BASE_DIR, "static_files/face_images/background_image.jpg")
+    item_destination_file_name = os.path.join(BASE_DIR, "static_files/items/item1.png")
+
+    final_image_directory = os.path.join(BASE_DIR, "static_files/output_images/final.png")
+    destination_blob_name = "output_images/final_image.jpg"
+    
+    
+    download_blob(bucket_name, background_source_blob_name,  item_source_blob_name, bg_destination_file_name , item_destination_file_name)
+
+    combine_images(item_destination_file_name , bg_destination_file_name , final_image_directory)
+
+    upload_blob(bucket_name, final_image_directory, destination_blob_name)
+
+
+
+
+    #print(*args)
+    #bg_destination_file_name = os.path.join(BASE_DIR, "imageCreator/face_images/downloadedFile.jpg")
+    #path = "F:\ImageProcessingModule\ImageProcessingModule\ImageProcessingModule\imageCreator\face_images\downloadedFile.jpg"
 
 
     html = "<html><body><h1>IMAGE: </h1><img src= '/static/face_images/background_image.jpg' > <img src= '/static/items/item1.png' ></body></html>"
@@ -56,3 +94,15 @@ def create(*args, **kwargs):
     return HttpResponse(html)
 
 
+def combine_images(item_image_path, bg_image_path, final_image_directory):
+
+    bg = Image.open(bg_image_path)
+    item = Image.open(item_image_path)
+    new_item_size = (370, 125)
+    item = item.resize(new_item_size).rotate(20, expand = True)
+    back_im = bg.copy()
+    back_im.paste(item, (260,340) , item)
+    back_im.save(final_image_directory, quality=95)
+
+
+    #return null
